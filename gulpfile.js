9bded6c04e5b9692c5c62a4eb6543419357b9165
Fred Boyle <fboyle@salesforce.com>
2021-07-08 16:17:24 -0700

    fix(snyk): Apply Snyk recommended fixes
    Signed-off-by: Fred Boyle <fboyle@salesforce.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,27 @@ const ignore = require('gulp-ignore')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 
-gulp.task('clean', () =>
-  del('.dist')
-)
+gulp.task('clean', () => del('.dist'))
 
-gulp.task('default', ['clean'], () =>
-  gulp
-    .src('lib/**/*.js')
-    .pipe(ignore.exclude(/__/))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(uglify({
-      preserveComments: 'license'
-    }))
-    .on('error', e => {
-      console.log(e)
-    })
-    .pipe(gulp.dest('.dist'))
+gulp.task(
+  'default',
+  gulp.series('clean', () =>
+    gulp
+      .src('lib/**/*.js')
+      .pipe(ignore.exclude(/__/))
+      .pipe(
+        babel({
+          presets: ['es2015']
+        })
+      )
+      .pipe(
+        uglify({
+          preserveComments: 'license'
+        })
+      )
+      .on('error', (e) => {
+        console.log(e)
+      })
+      .pipe(gulp.dest('.dist'))
+  )
 )
