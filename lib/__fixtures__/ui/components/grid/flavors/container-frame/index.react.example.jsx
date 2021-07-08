de8910bfc69e500c936dca1db0258f853fa75585
Fred Boyle <fboyle@salesforce.com>
2021-07-08 16:18:53 -0700

    Linting fixes
    Signed-off-by: Fred Boyle <fboyle@salesforce.com>

--- lib/__fixtures__/ui/components/grid/flavors/container-frame/index.react.example.jsx
+++ lib/__fixtures__/ui/components/grid/flavors/container-frame/index.react.example.jsx
@@ -9,20 +9,20 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-import React from 'react';
+import React from 'react'
 
 export const preview = (
-<div className="demo-only demo-container">
-  <div className="slds-grid slds-grid--frame slds-theme--shade" style={{height: '250px', margin: '-1rem'}}>
-    <div className="slds-col slds-grid slds-grid--align-center slds-grid--vertical-align-center"><div>App Content</div></div>
+  <div className='demo-only demo-container'>
+    <div className='slds-grid slds-grid--frame slds-theme--shade' style={{height: '250px', margin: '-1rem'}}>
+      <div className='slds-col slds-grid slds-grid--align-center slds-grid--vertical-align-center'><div>App Content</div></div>
+    </div>
   </div>
-</div>
-);
+)
 
 export const code = (
-<div className="demo-only demo-container">
-  <div className="slds-grid slds-grid--frame">
-    <div>App Content</div>
+  <div className='demo-only demo-container'>
+    <div className='slds-grid slds-grid--frame'>
+      <div>App Content</div>
+    </div>
   </div>
-</div>
-);
+)
--- lib/__fixtures__/ui/components/grid/flavors/order/index.react.example.jsx
+++ lib/__fixtures__/ui/components/grid/flavors/order/index.react.example.jsx
@@ -9,31 +9,30 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-import React from 'react';
-
+import React from 'react'
 
 export const preview = (
-<div className="demo-only demo-container">
-  <div className="slds-grid slds-wrap">
-    <div className="slds-col slds-p-horizontal--small slds-order--2 slds-medium-order--1 slds-large-order--3">
-      <div className="slds-box slds-box--small slds-theme--shade slds-text-align--center">1</div>
-    </div>
-    <div className="slds-col slds-p-horizontal--small slds-order--3 slds-medium-order--2 slds-large-order--2">
-      <div className="slds-box slds-box--small slds-theme--shade slds-text-align--center">2</div>
-    </div>
-    <div className="slds-col slds-p-horizontal--small slds-order--1 slds-medium-order--3 slds-large-order--1">
-      <div className="slds-box slds-box--small slds-theme--shade slds-text-align--center">3</div>
+  <div className='demo-only demo-container'>
+    <div className='slds-grid slds-wrap'>
+      <div className='slds-col slds-p-horizontal--small slds-order--2 slds-medium-order--1 slds-large-order--3'>
+        <div className='slds-box slds-box--small slds-theme--shade slds-text-align--center'>1</div>
+      </div>
+      <div className='slds-col slds-p-horizontal--small slds-order--3 slds-medium-order--2 slds-large-order--2'>
+        <div className='slds-box slds-box--small slds-theme--shade slds-text-align--center'>2</div>
+      </div>
+      <div className='slds-col slds-p-horizontal--small slds-order--1 slds-medium-order--3 slds-large-order--1'>
+        <div className='slds-box slds-box--small slds-theme--shade slds-text-align--center'>3</div>
+      </div>
     </div>
   </div>
-</div>
-);
+)
 
 export const code = (
-<div className="demo-only demo-container">
-  <div className="slds-grid">
-    <div className="slds-order--2 slds-medium-order--1 slds-large-order--3">1</div>
-    <div className="slds-order--3 slds-medium-order--2 slds-large-order--2">2</div>
-    <div className="slds-order--1 slds-medium-order--3 slds-large-order--1">3</div>
+  <div className='demo-only demo-container'>
+    <div className='slds-grid'>
+      <div className='slds-order--2 slds-medium-order--1 slds-large-order--3'>1</div>
+      <div className='slds-order--3 slds-medium-order--2 slds-large-order--2'>2</div>
+      <div className='slds-order--1 slds-medium-order--3 slds-large-order--1'>3</div>
+    </div>
   </div>
-</div>
-);
+)
